ca041916fb324a6a1f16a593bed8c87df11d5b18
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-09-01 14:50:56 -0300

    header and debounce

---
+++ src/components/main-header.tsx
@@ -0,0 +1,33 @@
+import type { ComponentProps } from "react";
+import Container from "./container";
+import Logo from "../assets/images/galeria-plus-full-logo.svg?react";
+import { Link } from "react-router";
+import cx from "classnames";
+import Button from "./button";
+import PhotoSearch from "./photo-search";
+import Divider from "./divider";
+
+interface MainHeaderProps extends ComponentProps<typeof Container> {}
+
+export default function MainHeader({ className, ...props }: MainHeaderProps) {
+  return (
+    <Container
+      as="header"
+      className={cx("flex justify-between items-center gap-10 mt-9", className)}
+      {...props}
+    >
+      <Link to="/">
+        <Logo className="h-5" />
+      </Link>
+
+      <PhotoSearch />
+
+      <Divider orientation="vertical" className="h-10" />
+
+      <div className="flex items-center gap-3">
+        <Button>Nova foto</Button>
+        <Button variant="secondary">Criar álbum</Button>
+      </div>
+    </Container>
+  );
+}
---
+++ src/helpers/utils.ts
@@ -0,0 +1,20 @@
+// biome-ignore lint/suspicious/noExplicitAny: any
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+) {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>): void => {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(later, wait);
+  };
+}
