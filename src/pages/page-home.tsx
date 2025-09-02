685cd1e98c0370cf069a209e2e77b0994195a16a
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-09-02 20:25:45 -0300

    photo list

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,23 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
-    <>
-      <Text variant="heading-medium">Home page</Text>
-    </>
+    <Container>
+      <PhotosList
+        photos={[
+          {
+            id: "123",
+            title: "Hello World!",
+            imageId: "square-breakfast.png",
+            albums: [
+              { id: "321", title: "Album 1" },
+              { id: "41", title: "Album 2" },
+              { id: "141", title: "Album 3" },
+            ],
+          },
+        ]}
+      />
+    </Container>
   );
 }
