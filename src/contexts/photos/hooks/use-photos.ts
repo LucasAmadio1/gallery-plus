3e5c9c707dfea4266020e07bd061a78f783bb0e9
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-09-10 12:36:40 -0300

    filters

--- src/contexts/photos/hooks/use-photos.ts
+++ src/contexts/photos/hooks/use-photos.ts
@@ -1,15 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import type { Photo } from "../models/photo";
 import { fetcher } from "../../../helpers/api";
+import { useQueryState, createSerializer, parseAsString } from "nuqs";
 
 export default function usePhotos() {
+  const [albumId, setAlbumId] = useQueryState("albumId");
+
+  const toSearchParams = createSerializer({ albumId: parseAsString });
+
   const { data, isLoading } = useQuery<Photo[]>({
-    queryKey: ["photos"],
-    queryFn: () => fetcher("/photos"),
+    queryKey: ["photos", albumId],
+    queryFn: () => fetcher(`/photos${toSearchParams({ albumId })}`),
   });
 
   return {
     photos: data || [],
     isLoadingPhotos: isLoading,
+    filters: {
+      albumId,
+      setAlbumId,
+    },
   };
 }
