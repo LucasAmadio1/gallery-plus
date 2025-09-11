01fc2d75a3fe461d26ea911319ea6c07834d307b
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-09-11 14:42:32 -0300

    photos navigator

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,49 +1,43 @@
-import { useParams } from "react-router";
-import Text from "../components/text";
 import Container from "../components/container";
-import type { Photo } from "../contexts/photos/models/photo";
+import Text from "../components/text";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
 import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 import useAlbums from "../contexts/albums/hooks/use-albums";
+import { useParams } from "react-router";
+import usePhoto from "../contexts/photos/hooks/use-photo";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
+  const { photo, isLoadingPhoto, nextPhotoId, previousPhotoId } = usePhoto(id);
   const { albums, isLoadingAlbums } = useAlbums();
-
-  const isLoadingPhoto = false;
-  const photo = {
-    id: "123",
-    title: "Hello World!",
-    imageId: "square-breakfast.png",
-    albums: [
-      { id: "321", title: "Album 1" },
-      { id: "41", title: "Album 2" },
-      { id: "141", title: "Album 3" },
-    ],
-  } as Photo;
 
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
           <Text as="h2" variant="heading-large">
-            {photo.title}
+            {photo?.title}
           </Text>
         ) : (
           <Skeleton className="w-48 h-8" />
         )}
 
-        <PhotosNavigator loading={isLoadingPhoto} />
+        <PhotosNavigator
+          previousPhotoId={previousPhotoId}
+          nextPhotoId={nextPhotoId}
+          loading={isLoadingPhoto}
+        />
       </header>
 
       <div className="grid grid-cols-[21rem_1fr] gap-24">
-        <div className="flex flex-col gap-3">
+        <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
-              src={`/images/square-breakfast.png`}
+              src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
               title={photo?.title}
               imageClassName="h-[21rem]"
             />
@@ -51,22 +45,19 @@ export default function PagePhotoDetails() {
             <Skeleton className="h-[21rem]" />
           )}
 
-          <div>
-            {!isLoadingPhoto ? (
-              <Button variant="destructive">Excluir</Button>
-            ) : (
-              <Skeleton className="h-10 w-20" />
-            )}
-          </div>
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
         </div>
 
         <div className="py-3">
           <Text as="h3" variant="heading-medium" className="mb-6">
             Álbuns
           </Text>
-
           <AlbumsListSelectable
-            photo={photo}
+            photo={photo as Photo}
             albums={albums}
             loading={isLoadingAlbums}
           />
