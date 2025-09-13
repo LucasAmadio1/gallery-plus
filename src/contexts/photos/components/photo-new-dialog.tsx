d85782518ee91ee6eecdc6fbb49de21a73f5a7b9
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-09-13 16:47:55 -0300

    create photo

--- src/contexts/photos/components/photo-new-dialog.tsx
+++ src/contexts/photos/components/photo-new-dialog.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import {
   Dialog,
   DialogBody,
@@ -17,73 +17,136 @@ import Text from "../../../components/text";
 import Skeleton from "../../../components/skeleton";
 import { useForm } from "react-hook-form";
 import useAlbums from "../../albums/hooks/use-albums";
+import { photoNewFormSchema, type PhotoNewFormSchema } from "../../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import usePhoto from "../hooks/use-photo";
 
 interface PhotoNewDialogProps {
   trigger: React.ReactNode;
 }
 
 export default function PhotoNewDialog({ trigger }: PhotoNewDialogProps) {
-  const form = useForm();
+  const [openModal, setOpenModal] = React.useState(false);
+
+  const form = useForm<PhotoNewFormSchema>({
+    resolver: zodResolver(photoNewFormSchema),
+  });
+
+  const { createPhoto } = usePhoto();
   const { albums, isLoadingAlbums } = useAlbums();
+  const [isCreatingPhoto, setIsCreatingPhoto] = React.useTransition();
+
+  const file = form.watch("file");
+  const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
+
+  const albumsIds = form.watch("albumsIds");
+
+  React.useEffect(() => {
+    if (!openModal) {
+      form.reset();
+    }
+  }, [openModal, form]);
+
+  function handleToggleAlbum(albumId: string) {
+    const albumsIds = form.getValues("albumsIds") || [];
+    const albumsSet = new Set(albumsIds);
+
+    if (albumsSet.has(albumId)) {
+      albumsSet.delete(albumId);
+    } else {
+      albumsSet.add(albumId);
+    }
+
+    form.setValue("albumsIds", Array.from(albumsSet));
+  }
+
+  function handleSubmit(payload: PhotoNewFormSchema) {
+    setIsCreatingPhoto(async () => {
+      await createPhoto(payload);
+      setOpenModal(false);
+    });
+  }
 
   return (
-    <Dialog>
+    <Dialog open={openModal} onOpenChange={setOpenModal}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
 
       <DialogContent>
-        <DialogHeader>Adicionar foto</DialogHeader>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <DialogHeader>Adicionar foto</DialogHeader>
 
-        <DialogBody className="flex flex-col gap-5">
-          <InputText placeholder="Adicione um título" maxLength={255} />
+          <DialogBody className="flex flex-col gap-5">
+            <InputText
+              placeholder="Adicione um título"
+              maxLength={255}
+              error={form.formState.errors.title?.message}
+              {...form.register("title")}
+            />
 
-          <Alert>
-            Tamanho máximo: 50MB
-            <br />
-            Você pode selecionar arquivos em PNG, JPG ou JPEG
-          </Alert>
+            <Alert>
+              Tamanho máximo: 50MB
+              <br />
+              Você pode selecionar arquivos em PNG, JPG ou JPEG
+            </Alert>
 
-          <InputSingleFile
-            form={form}
-            allowedExtensions={["png", "jpg", "jpeg"]}
-            maxFileSizeInMB={50}
-            replaceBy={<ImagePreview className="w-full h-56" />}
-          />
+            <InputSingleFile
+              form={form}
+              allowedExtensions={["png", "jpg", "jpeg"]}
+              maxFileSizeInMB={50}
+              replaceBy={
+                <ImagePreview src={fileSource} className="w-full h-56" />
+              }
+              error={form.formState.errors.file?.message}
+              {...form.register("file")}
+            />
 
-          <div className="flex flex-col gap-3">
-            <Text variant="label-small">Selecionar álbum</Text>
+            <div className="flex flex-col gap-3">
+              <Text variant="label-small">Selecionar álbum</Text>
 
-            <div className="flex flex-wrap gap-3">
-              {!isLoadingAlbums &&
-                albums.length > 0 &&
-                albums.map((album) => (
-                  <Button
-                    key={album.id}
-                    variant="ghost"
-                    size="sm"
-                    className="truncate"
-                  >
-                    {album.title}
-                  </Button>
-                ))}
+              <div className="flex flex-wrap gap-3">
+                {!isLoadingAlbums &&
+                  albums.length > 0 &&
+                  albums.map((album) => (
+                    <Button
+                      key={album.id}
+                      variant={
+                        albumsIds?.includes(album.id) ? "primary" : "ghost"
+                      }
+                      size="sm"
+                      className="truncate"
+                      onClick={() => handleToggleAlbum(album.id)}
+                    >
+                      {album.title}
+                    </Button>
+                  ))}
 
-              {isLoadingAlbums &&
-                Array.from({ length: 5 }).map((_, index) => (
-                  <Skeleton
-                    key={`album-loading-${index}`}
-                    className="w-20 h-7"
-                  />
-                ))}
+                {isLoadingAlbums &&
+                  Array.from({ length: 5 }).map((_, index) => (
+                    <Skeleton
+                      key={`album-loading-${index}`}
+                      className="w-20 h-7"
+                    />
+                  ))}
+              </div>
             </div>
-          </div>
-        </DialogBody>
+          </DialogBody>
 
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="secondary">Cancelar</Button>
-          </DialogClose>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button disabled={isCreatingPhoto} variant="secondary">
+                Cancelar
+              </Button>
+            </DialogClose>
 
-          <Button>Adicionar</Button>
-        </DialogFooter>
+            <Button
+              disabled={isCreatingPhoto}
+              handling={isCreatingPhoto}
+              type="submit"
+            >
+              {isCreatingPhoto ? "Adicionando..." : "Adicionar"}
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -1,6 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetcher } from "../../../helpers/api";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
+import type { PhotoNewFormSchema } from "../../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -8,16 +10,52 @@ interface PhotoDetailResponse extends Photo {
 }
 
 export default function usePhoto(id?: string) {
+  const queryClient = useQueryClient();
+
   const { data, isLoading } = useQuery<PhotoDetailResponse>({
     queryKey: ["photo", id],
     queryFn: () => fetcher(`/photos/${id}`),
     enabled: !!id,
   });
 
+  async function createPhoto(payload: PhotoNewFormSchema) {
+    try {
+      const { data: photo } = await api.post<Photo>("/photos", {
+        title: payload.title,
+      });
+
+      await api.post(
+        `/photos/${photo.id}/image`,
+        {
+          file: payload.file[0],
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+
+      if (payload.albumsIds && payload.albumsIds.length > 0) {
+        await api.put(`/photos/${photo.id}/albums`, {
+          albumsIds: payload.albumsIds,
+        });
+      }
+
+      queryClient.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Foto criada com sucesso!");
+    } catch (error) {
+      toast.success("Erro ao criar foto");
+      console.log(error);
+    }
+  }
+
   return {
     photo: data,
     nextPhotoId: data?.nextPhotoId,
     previousPhotoId: data?.previousPhotoId,
     isLoadingPhoto: isLoading,
+    createPhoto,
   };
 }
